0acd459ca1948dd3e24614032a1e7b43b8fd7688
sethyadav <sethyadav169@gmail.com>
2026-02-05 13:15:47 +0530

    day-7 | done

---
+++ day-7/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+const connectToDb = require("./src/config/database")
+
+const app = require("./src/app")
+
+
+
+connectToDb()
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
