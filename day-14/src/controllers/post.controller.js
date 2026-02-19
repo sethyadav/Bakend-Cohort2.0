5765cc735c5f316f4a8ed0d46a37d08c6705757c
sethyadav <sethyadav169@gmail.com>
2026-02-19 12:22:44 +0530

    middlewares

--- day-14/src/controllers/post.controller.js
+++ day-14/src/controllers/post.controller.js
@@ -10,25 +10,9 @@ const imageKit = new ImageKit({
 })
 
 async function createPostController(req, res) {
-    console.log(req.body, req.file)
+    
 
-    const token = req.cookies.token
-
-    if(!token) {
-        return req.cookies(401).json({
-           message: "Token not provided, Unauthorized"
-        })
-    }
-
-    let decoded = null
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (err) {
-        return res.status(401).json ({
-            message: "user not authorized"
-        })
-    }    
-    console.log(decoded)
+   
 
     const file = await imageKit.files.upload ({
         file: await toFile(Buffer.from(req.file.buffer),'file'),
@@ -39,7 +23,7 @@ async function createPostController(req, res) {
     const post = await postModel.create({
         caption: req.body.caption,
         imageUrl: file.url,
-        user: decoded.id
+        user: req.user.id
     })
 
     res.status(201).json({
@@ -53,7 +37,57 @@ async function createPostController(req, res) {
  
 }
 
+async function getPostController(req,res) {
+
+   
+
+  const userId  = req.user.id
+
+  const posts  = await postModel.find({
+    user:userId
+  })
+
+  res.status(200)
+  .json ({
+    message: "Posts fetched successfully",
+    posts
+  })
+
+}
+
+async function getPostDetailsController(req, res) {
+
+    
+
+    const userId = req.user.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(!post) {
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden Content."
+        })
+    }
+
+    return res.status(200).json ({
+        message:"Post fetchrd successfully.",
+        post
+    })
+}
+
+
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
 
--- day-14/src/routes/post.routes.js
+++ day-14/src/routes/post.routes.js
@@ -3,11 +3,28 @@ const postRouter = express.Router()
 const PostController = require("../controllers/post.controller")
 const multer = require("multer")
 const upload = multer({ storage: multer.memoryStorage() })
+const identifyUser = require("../middlewares/auth.middleware")
+
+
 /**
  * Post /api/posts [protected]
  * -req.body = { caption,image-file }
  */
 
-postRouter.post('/',upload.single("image"), PostController.createPostController)
+postRouter.post('/',upload.single("image"), identifyUser, PostController.createPostController)
+
+/**
+ * GET /api/posts/ [protected]
+ */
+
+postRouter.get("/", identifyUser, PostController.getPostController)
+
+/**
+ * GET  /api/posts/details/:postid
+ * - return an detail about specific posts with the id. also check whether the posts 
+ * bleong to user that request come from
+ */
+
+postRouter .get ("/details/:postId",PostController.getPostDetailsController)
 
 module.exports = postRouter
